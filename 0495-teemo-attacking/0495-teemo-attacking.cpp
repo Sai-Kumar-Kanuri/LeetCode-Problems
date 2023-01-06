bc5590f7a610dd4468f5bf6c390f286e6a778a63
Sai Kumar Kanuri <92247737+Sai-Kumar-Kanuri@users.noreply.github.com>
2023-01-06 15:39:53 +0530

    Time: 44 ms (84.94%), Space: 25.8 MB (92.47%) - LeetHub

---
+++ 0495-teemo-attacking/0495-teemo-attacking.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int findPoisonedDuration(vector<int>& timeSeries, int duration) {
+        int ans=0;
+        for(int i=0;i<timeSeries.size()-1;i++)
+        {
+            if(timeSeries[i]+duration>timeSeries[i+1])
+            {
+                ans=ans+timeSeries[i+1]-timeSeries[i];
+            }
+            else{
+                 ans+=duration;
+            }  
+        }
+        return ans+duration;
+    }
+};
