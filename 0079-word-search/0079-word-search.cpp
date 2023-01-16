a5a82bcd0f96e46fbebde96d8e10c54b56d491a9
Sai Kumar Kanuri <92247737+Sai-Kumar-Kanuri@users.noreply.github.com>
2023-01-16 18:36:12 +0530

    Time: 1078 ms (27.44%), Space: 8.1 MB (48.73%) - LeetHub

---
+++ 0079-word-search/0079-word-search.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool search(vector<vector<char>>&board,string word,int i,int j,int pos){
+        if(pos==word.length()){
+            return true;
+        }
+        
+        if(i<0 || j<0 || i>=board.size() || j>=board[0].size()){
+            return false;
+        }
+        
+        if(board[i][j]==word[pos]){
+            board[i][j]='*';
+            if(search(board,word,i+1,j,pos+1) || search(board,word,i-1,j,pos+1) || search(board,word,i,j+1,pos+1) || search(board,word,i,j-1,pos+1) ){
+                return true;
+            }
+            board[i][j]=word[pos];
+            return false;
+        }
+        return false;
+    }
+    bool exist(vector<vector<char>>& board, string word) {
+        for(int i=0;i<board.size();i++){
+            for(int j=0;j<board[i].size();j++){
+                cout<<i<<"-"<<j<<endl;
+                if(search(board,word,i,j,0)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
