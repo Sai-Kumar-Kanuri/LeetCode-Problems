5b164898d4483d664c204fdac465facb4b1b41cc
Sai Kumar Kanuri <92247737+Sai-Kumar-Kanuri@users.noreply.github.com>
2023-01-22 21:46:01 +0530

    Time: 155 ms (55.72%), Space: 57.5 MB (59.05%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,35 +1,5 @@
 class Solution {
 public:
-//    void getPartition(string s,int idx,vector<string>&path,vector<vector<string>>&ans){
-//         if(s.size()==idx){
-//             ans.push_back(path);
-//             return;
-//         }
-        
-//         for(int i=idx;i<s.size();++i){
-//             if(isPalindrome(s,idx,i)){
-//                 path.push_back(s.substr(idx,i-idx+1));
-//                 getPartition(s,idx+1,path,ans);
-//                 path.pop_back();
-//             }
-//         }
-//     }
-
-//  bool isPalindrome(string s,int start,int end){
-//         while(start<=end){
-//             if(s[start++]!=s[end--]){
-//                 return false;
-//             }
-//         }
-//         return true;
-//     }
-//     vector<vector<string>> partition(string s) {
-//         vector<string>path;
-//         vector<vector<string>>ans;
-//         getPartition(s,0,path,ans);
-//         return ans;
-        
-//     }
     vector < vector < string >> partition(string s) {
       vector < vector < string > > res;
       vector < string > path;
