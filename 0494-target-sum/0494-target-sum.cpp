ebdc436ca9aa8675230baea19c205b1f1472a24c
Sai Kumar Kanuri <92247737+Sai-Kumar-Kanuri@users.noreply.github.com>
2023-01-26 10:33:52 +0530

    Time: 17 ms (73.76%), Space: 12.1 MB (48.08%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,22 +1,51 @@
-
 class Solution {
 public:
-    int findTargetSumWays(vector<int>& nums, int target) {
-        int count=0;
-        getCalculation(nums,target,0,0,count);
-        return count;
+    int f(int ind,vector<vector<int>>&dp,vector<int>&arr,int target){
+
+    if(ind==0){
+      if (target == 0 && arr[0] == 0) {
+        return 2;
+      }
+        if(target==0 || target == arr[0])
+            return 1;
+        return 0;
     }
-    void getCalculation(vector<int>&nums,int target,int sum,int i,int &count)
-    {
-        if(nums.size()==i){
-            if(sum==target){
-                count++;
-            }
-        }
-        else{
-            getCalculation(nums,target,sum+nums[i],i+1,count);
-            getCalculation(nums,target,sum-nums[i],i+1,count);
-        }
+
+    if(dp[ind][target]!=-1)
+        return dp[ind][target];
         
+    int notTaken = f(ind-1,dp,arr,target);
+    
+    int taken = 0;
+    if(arr[ind]<=target)
+        taken = f(ind-1,dp,arr,target-arr[ind]);
+        
+    return dp[ind][target]= (notTaken + taken);
+}
+int countPartitions(int n, int d, vector<int> &arr) {
+    // Write your code here.
+    // cout<<n<<"n"<<"d"<<d<<endl;
+    int totsum=0;
+    for(auto x:arr){
+        totsum+=x;
+    }
+    
+    if((totsum-d)<0){
+        return 0;
+    }
+
+    if((totsum-d)%2){
+        return 0;
+    }
+
+    int target=(totsum-d)/2;
+    // cout<<"target"<<target<<endl;
+    vector<vector<int>>dp(n,vector<int>(target+1,-1));
+    return f(n-1,dp,arr,target);
+
+
+}
+    int findTargetSumWays(vector<int>& nums, int target) {
+        return countPartitions(nums.size(),target,nums);
     }
 };
