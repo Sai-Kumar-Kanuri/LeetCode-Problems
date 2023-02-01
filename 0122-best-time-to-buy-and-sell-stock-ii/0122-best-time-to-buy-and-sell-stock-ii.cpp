f4fd4b25f339cf8a83dfa257760ec0c896ef3608
Sai Kumar Kanuri <92247737+Sai-Kumar-Kanuri@users.noreply.github.com>
2023-02-01 18:14:50 +0530

    Time: 15 ms (31.33%), Space: 15.3 MB (21.51%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -22,7 +22,24 @@ public:
         return dp[ind][buy]=profit;
     }
     int maxProfit(vector<int>& prices) {
-        vector<vector<int>>dp(prices.size(),vector<int>(2,-1));
-        return f(0,1,prices,dp); 
+        vector<vector<int>>dp(prices.size()+1,vector<int>(2,0));
+        dp[prices.size()][0]=dp[prices.size()][1]=0;
+        int profit=0;
+
+        for(int ind=prices.size()-1;ind>=0;ind--){
+            for(int buy=0;buy<=1;buy++){
+                if(buy){
+                    profit=max(-prices[ind]+(dp[ind+1][0]),0+dp[ind+1][1]);
+                }
+                else{
+                    profit=max(prices[ind]+dp[ind+1][1],0+dp[ind+1][0]);
+                }
+                
+                dp[ind][buy]=profit;
+            }
+        }
+        
+        return dp[0][1];
+        // return f(0,1,prices,dp);  
     }
 };
