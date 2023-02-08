bd76740a3883ceec603e1626ec939db0ea785cbf
Sai Kumar Kanuri <92247737+Sai-Kumar-Kanuri@users.noreply.github.com>
2023-02-08 17:42:35 +0530

    Time: 60 ms (35.44%), Space: 37.2 MB (18.37%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.cpp
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -15,21 +15,10 @@ public:
             return dp[i];
         }
         
-        
-        // bool left=false,right=false;
-        // if(i+arr[i]<arr.size()){
-        //     // int val=arr[i];
-        //     // arr[i]=-1;
             vis[i]=1;
            bool right=f(i+arr[i],arr,dp,vis);
-        // }
-        
-//         else if(i-arr[i]>=0){
-//             // int val=arr[i];
-//             // arr[i]=-1;
-//             vis[i]=1;
+
            bool left=f(i-arr[i],arr,dp,vis);
-        // }
         
         return dp[i]=left||right;
         
