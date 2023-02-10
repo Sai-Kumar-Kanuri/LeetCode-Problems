4c69511bd19d9ca06c64a9a588ff9c8bafd410d2
Sai Kumar Kanuri <92247737+Sai-Kumar-Kanuri@users.noreply.github.com>
2023-02-10 23:03:42 +0530

    Time: 22 ms (88.14%), Space: 15.1 MB (20.06%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    void dfs(int node,vector<int>adjLs[], int vis[]){
+    void dfs(int node,vector<int>adjLs[], vector<int>&vis){
         vis[node]=1;
         
         for(auto it :  adjLs[node]){
@@ -24,10 +24,12 @@ public:
             }
         }
         cout<<V<<endl;
-        int vis[V];
-        for(int i=0;i<V;i++){
-            vis[i]=0;
-        }
+        // int vis[V];
+        // for(int i=0;i<V;i++){
+        //     vis[i]=0;
+        // }
+        // memset(vis,0,V);
+        vector<int>vis(V,0);
         int cnt=0;
         for(int i=0;i<V;i++){
             if(!vis[i]){
