245b97cfff96500da61509ef56b5f6403739a3c7
Sai Kumar Kanuri <92247737+Sai-Kumar-Kanuri@users.noreply.github.com>
2023-03-12 21:47:57 +0530

    Time: 23 ms (29.04%), Space: 13.8 MB (51.96%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    static bool compare(pair<int,int>p1,pair<int,int>p2)
+    {
+        if(p1.second==p2.second)
+        {
+            return p1.first>p2.first;
+        }
+        
+        return p1.second>p2.second;
+    }
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int,int>mp;
+        
+        for(auto it:nums)
+        {
+            mp[it]++;
+        }
+        
+        vector<pair<int, int> > ans(mp.begin(), mp.end());
+        
+        sort(ans.begin(),ans.end(),compare);
+        
+        vector<int>final;
+        
+        for(int i=0;i<k;i++)
+        {
+            final.push_back(ans[i].first);
+        }
+        return final;;
+    }
+};
