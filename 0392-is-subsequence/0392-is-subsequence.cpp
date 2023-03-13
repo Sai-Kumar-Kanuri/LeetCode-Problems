f1c023c181461b35aacc0d73fe8769cc9666cb63
Sai Kumar Kanuri <92247737+Sai-Kumar-Kanuri@users.noreply.github.com>
2023-03-13 14:15:33 +0530

    Time: 71 ms (5.02%), Space: 282.4 MB (5.15%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    bool f(int i,int j,string s,string t)
+    bool f(int i,int j,string s,string t,vector<vector<int>>&dp)
     {
         if(i==s.length())
         {
@@ -12,15 +12,22 @@ public:
             return false;
         }
         
-        if(s[i]==t[j])
+        if(dp[i][j]!=-1)
         {
-            return f(i+1,j+1,s,t);
+            return dp[i][j];
         }
         
-        return f(i,j+1,s,t);
+        if(s[i]==t[j])
+        {
+            return dp[i][j]=f(i+1,j+1,s,t,dp);
+        }
+        
+        return dp[i][j]=f(i,j+1,s,t,dp);
     }
     bool isSubsequence(string s, string t) {
-        
-        return f(0,0,s,t);
+        int n=s.length();
+        int m=t.length();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+        return f(0,0,s,t,dp);
     }
 };
