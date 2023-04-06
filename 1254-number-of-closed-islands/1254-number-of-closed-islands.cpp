0398e0a1968a60ff62c647dc855f8cf6c4634e63
Sai Kumar Kanuri <92247737+Sai-Kumar-Kanuri@users.noreply.github.com>
2023-04-07 01:13:28 +0530

    Time: 19 ms (24.68%), Space: 14.4 MB (9.56%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
@@ -34,6 +34,42 @@ public:
         
         return isClosed;
     }
+    
+    int bfs(int row,int col,vector<vector<int>>& grid,vector<vector<int>>&vis)
+    {
+        int m=grid.size();
+        int n=grid[0].size();
+        vis[row][col]=1;
+        
+        queue<pair<int,int>>q;
+        q.push({row,col});
+        int isClosed=1;
+        int drow[]={-1,0,1,0};
+        int dcol[]={0,1,0,-1};
+        while(!q.empty())
+        {
+            auto it=q.front();
+            q.pop();
+            int r=it.first;
+            int c=it.second;
+            
+            for(int i=0;i<4;i++)
+            {
+                int nrow=r+drow[i];
+                int ncol=c+dcol[i];
+                
+                if(nrow<0 || nrow>=m || ncol<0 || ncol>=n)
+                {
+                    isClosed=0;
+                }
+                else if(grid[nrow][ncol]==0 && !vis[nrow][ncol]){
+                    q.push({nrow,ncol});
+                    vis[nrow][ncol]=1;
+                }
+            }
+        }
+        return isClosed;
+    }
     int closedIsland(vector<vector<int>>& grid) {
         
         int m=grid.size();
@@ -45,7 +81,7 @@ public:
         {
             for(int j=0;j<n;j++)
             {
-                if(grid[i][j]==0 && !vis[i][j] && dfs(i,j,grid,vis))
+                if(grid[i][j]==0 && !vis[i][j] && bfs(i,j,grid,vis))
                 {
                     count++;
                 }
