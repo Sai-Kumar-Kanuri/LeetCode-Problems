22a6617eb68c6d635da0462a0f30b861c1acf8b6
Sai Kumar Kanuri <92247737+Sai-Kumar-Kanuri@users.noreply.github.com>
2023-06-02 18:34:27 +0530

    Time: 84 ms (46.97%), Space: 21.7 MB (53.46%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -2,18 +2,15 @@ class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         int n=grid.size();
-        
-        
-        
-        if(grid[0][0]==1)
-        {
+        // int m=grid[0].size();
+        if(grid[0][0]==1){
             return -1;
         }
-        if(n==1)
-        {
+        
+        if(n==1){
             return 1;
         }
-      
+        
         queue<pair<int,pair<int,int>>>q;
         q.push({1,{0,0}});
         
@@ -26,24 +23,26 @@ public:
             int dis=it.first;
             int row=it.second.first;
             int col=it.second.second;
-       
+            
             for(int i=-1;i<=1;i++)
+            {
                 for(int j=-1;j<=1;j++)
                 {
                     int nrow=row+i;
-                    int ncol=col+j;
+                    int ncol= col+j;
                     
-                    if(nrow>=0 && nrow<n && ncol<n && ncol>=0 && grid[nrow][ncol]==0 && dis+1<dist[nrow][ncol])
-                    {
+                    if(nrow>=0 && nrow<n && ncol>=0 && ncol<n && grid[nrow][ncol]==0 && dis+1<dist[nrow][ncol]){
                         dist[nrow][ncol]=dis+1;
-                        if(nrow==n-1 && ncol==n-1)
-                        {
+                        if(nrow==n-1 && ncol==n-1){
                             return dis+1;
                         }
                         q.push({dist[nrow][ncol],{nrow,ncol}});
-                    }                    
-                }    
+                    }
+                }
+            }
         }
         return -1;
+        
+        
     }
 };
