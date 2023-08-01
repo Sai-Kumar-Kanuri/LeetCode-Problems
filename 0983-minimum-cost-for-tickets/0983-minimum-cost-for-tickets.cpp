5a41f8a81c097d1d541cfe54598984ce34cbbb39
Sai Kumar Kanuri <92247737+Sai-Kumar-Kanuri@users.noreply.github.com>
2023-08-01 12:20:30 +0530

    Time: 0 ms (100.00%), Space: 9.6 MB (77.03%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
@@ -1,44 +1,41 @@
 class Solution {
 public:
-    int f(vector<int>& days, vector<int>& costs,int idx,vector<int>&dp)
-    {
-        if(idx==days.size())
-        {
+    
+    int f(int idx,vector<int>& days, vector<int>& costs,vector<int>& dp){
+        
+        if(idx>=days.size()){
             return 0;
         }
-
-        if(dp[idx]!=-1)
-        {
+        
+        if(dp[idx]!=-1){
             return dp[idx];
         }
-
-        int take1=costs[0]+f(days,costs,idx+1,dp);
-
-        int num=days[idx]+7;
-        int idx7=idx;
-
-        while(idx7<days.size() && days[idx7]<num )
-        {
+        
+        int pick1 = costs[0]+f(idx+1,days,costs,dp);;
+        
+        
+        int num = days[idx]+7;
+        int idx7 = idx;
+        while(idx7<days.size() && days[idx7]<num){
             idx7++;
         }
-
-        int take7=costs[1]+f(days,costs,idx7,dp);
-
-        num=days[idx]+30;
-        int idx30=idx;
-
-        while(idx30<days.size() && days[idx30]<num )
-        {
+        
+        int pick7 = costs[1]+f(idx7,days,costs,dp);
+        
+        int num1 = days[idx]+30;
+        int idx30 = idx;
+        while(idx30<days.size() && days[idx30]<num1){
             idx30++;
         }
-
-        int take30=costs[2]+f(days,costs,idx30,dp);
-
-        return dp[idx]=min(take1,min(take7,take30));
-
+        
+        int pick30 = costs[2]+f(idx30,days,costs,dp);
+        
+        
+        return dp[idx]=min({pick1,pick7,pick30});
     }
     int mincostTickets(vector<int>& days, vector<int>& costs) {
         vector<int>dp(days.size()+1,-1);
-        return f(days,costs,0,dp);
+        
+        return f(0,days,costs,dp);
     }
 };
