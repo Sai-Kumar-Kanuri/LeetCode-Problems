cdea962a41bb75003d0d7c6133b429f0fc53a828
Sai Kumar Kanuri <92247737+Sai-Kumar-Kanuri@users.noreply.github.com>
2023-08-10 08:34:29 +0530

    Time: 15 ms (96.42%), Space: 13.7 MB (41.30%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,51 +1,53 @@
 class Solution {
 public:
-    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int K) {
-        vector<pair<int,int>>adj[n];
-        for(auto it: flights)
-        {
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        
+        
+        vector<pair<int,int>>adj[n];//{to,cost}
+        
+        for(auto it:flights){
             adj[it[0]].push_back({it[1],it[2]});
         }
         
-        queue<pair<int,pair<int,int>>>q; //{stops,{node,dist}}
-        vector<int> dist(n, 1e9);
-        dist[src] = 0;
+        vector<int>dist(n,1e9);
+        
+        
+        queue<pair<int,pair<int,int>>>q;//{stops,{node,dist}};
+        
+        dist[src]=0;
         q.push({0,{src,0}});
         
-        while(!q.empty())
-        {
+        
+        while(!q.empty()){
+            
             auto it = q.front();
-            int node=it.second.first;
+            
+            int node = it.second.first;
             int dis = it.second.second;
             
             int stops = it.first;
-            
             q.pop();
             
-            if(stops>K)
-            {
+            if(stops>k){
                 continue;
             }
             
-            for(auto it:adj[node])
-            {
-                int adjNode=it.first;
-                int edgW=it.second;
+            for(auto it : adj[node]){
+                int adjNode = it.first;
+                int cost = it.second;
                 
-                if(dis+edgW < dist[adjNode] && stops<=K)
-                {
-                    dist[adjNode]=dis+edgW;
-                    q.push({stops+1,{adjNode,dis+edgW}});
+                if(dis+cost<dist[adjNode] && stops<=k){
+                    dist[adjNode]=dis+cost;
+                    q.push({stops+1,{adjNode,dis+cost}});
                 }
             }
         }
         
-        if(dist[dst]==1e9)
-        {
+        if(dist[dst]==1e9){
             return -1;
         }
         
-        return dist[dst];
+        return dist[dst] ;
         
     }
 };
