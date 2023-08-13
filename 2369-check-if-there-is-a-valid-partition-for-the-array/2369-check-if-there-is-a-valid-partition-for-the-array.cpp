e2022e181c4cb696fec78bdf9d7eb4ce2d2af96e
Sai Kumar Kanuri <92247737+Sai-Kumar-Kanuri@users.noreply.github.com>
2023-08-13 11:05:06 +0530

    Time: 142 ms (76.61%), Space: 92.9 MB (22.46%) - LeetHub

--- 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.cpp
+++ 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.cpp
@@ -11,25 +11,22 @@ public:
             return dp[i];
         }
         
-        bool ans1=false,ans2=false,ans3=false;
+        // bool ans1=false,ans2=false,ans3=false;
+        bool res=false;
         
         if(i+1<nums.size() && nums[i]==nums[i+1]){
-            ans1 = f(i+2,nums,dp);
+            res = f(i+2,nums,dp);
         }
         
         if(i+2<nums.size() && nums[i]==nums[i+1] && nums[i]==nums[i+2]){
-            ans2 = f(i+3,nums,dp);
+            res = res || f(i+3,nums,dp);
         }
         
         if(i+2<nums.size() && nums[i]==nums[i+1]-1 && nums[i+1]==nums[i+2]-1){
-            ans3 = f(i+3,nums,dp);
+            res = res || f(i+3,nums,dp);
         }
         
-        if( ans1 || ( ans2 || ans3 )){
-            return dp[i]=true;
-        }
-        
-        return dp[i]=false;
+        return dp[i]=res;
         
     }
     bool validPartition(vector<int>& nums) {
