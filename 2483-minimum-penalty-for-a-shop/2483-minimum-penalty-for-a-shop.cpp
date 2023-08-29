45fbdedc76a112cffc4791c6cf8b502cfa5e8c7f
Sai Kumar Kanuri <92247737+Sai-Kumar-Kanuri@users.noreply.github.com>
2023-08-29 17:37:26 +0530

    Time: 36 ms (40.54%), Space: 18.9 MB (15.96%) - LeetHub

--- 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.cpp
+++ 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.cpp
@@ -1,30 +1,36 @@
 class Solution {
 public:
     int bestClosingTime(string customers) {
-        int curPenalty = count(customers.begin(), customers.end(), 'Y');
-        int minPenalty = curPenalty;
-        int earliestHour = 0;
+        int n = customers.length();
         
-        cout<<minPenalty<<endl;
-
-        for (int i = 0; i < customers.size(); i++) {
-            char ch = customers[i];
-            
-            if (ch == 'Y') {
-                curPenalty--;
-            } else {
-                curPenalty++;
-            }
-            
-            cout<<i<<" "<<curPenalty<<endl;
-
-            // Update earliestHour if a smaller penatly is encountered.
-            if (curPenalty < minPenalty) {
-                earliestHour = i + 1;
-                minPenalty = curPenalty;
+        vector<int>prefix_n(n+1,0);
+        vector<int>postfix_y(n+1,0);
+        
+        for(int i=1;i<=n;i++){
+            prefix_n[i]=prefix_n[i-1];
+            if(customers[i-1]=='N'){
+                prefix_n[i]+=1;
             }
         }
-
-        return earliestHour;
+        
+        for(int i=n-1;i>=0;i--){
+            postfix_y[i]=postfix_y[i+1];
+            if(customers[i]=='Y'){
+                postfix_y[i]+=1;
+            }
+        }
+        
+        int minPenalty=1e9;
+        int idx=0;
+        for(int i=0;i<n+1;i++){
+            int curPenalty=prefix_n[i]+postfix_y[i];
+            
+            if(curPenalty<minPenalty){
+                minPenalty=curPenalty;
+                idx=i;
+            }
+        }
+        
+        return idx;
     }
 };
