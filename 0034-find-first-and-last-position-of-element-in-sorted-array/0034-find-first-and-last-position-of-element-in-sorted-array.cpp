5c8fa34620be37243b2003ab889a828e2fdd9f00
Sai Kumar Kanuri <92247737+Sai-Kumar-Kanuri@users.noreply.github.com>
2023-10-14 22:07:37 +0530

    Time: 11 ms (8.91%), Space: 13.9 MB (47.08%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,44 +1,38 @@
 class Solution {
 public:
-    
-    int searchIndex(vector<int>&nums,int target,bool findStartIndex)
-    {
-         int start=0;
+    int f(vector<int>& nums, int target,bool flag){
+        int low=0;
+        int high=nums.size()-1;
         int ans=-1;
-        int end=nums.size()-1;
-        
-        while(start<=end)
-        {
-            int mid = start+(end-start)/2;
+        while(low<=high){
+            int mid = (low+high)/2;
             
-            if(target<nums[mid])
-            {
-                end=mid-1;
+            if(target<nums[mid]){
+                high=mid-1;
             }
             
             else if(target>nums[mid]){
-                start=mid+1;
+                low=mid+1;
             }
+            
             else{
                 ans=mid;
-                if(findStartIndex)
-                {
-                    end=mid-1;
-                }
-                else{
-                    start=mid+1;
+                if(flag){
+                    high=mid-1;
+                }else{
+                    low=mid+1;
                 }
             }
         }
+        
         return ans;
     }
-    
     vector<int> searchRange(vector<int>& nums, int target) {
-       vector<int>ans(2,-1);
         
-        ans[0]=searchIndex(nums,target,true);
-        ans[1]=searchIndex(nums,target,false);
-        
+        vector<int>ans(2);
+        ans[0]=f(nums,target,true);
+        ans[1]=f(nums,target,false);
         return ans;
+        
     }
 };
