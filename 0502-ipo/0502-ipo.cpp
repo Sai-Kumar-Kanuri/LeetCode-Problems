5596a6dacf2ed6d4ab87a8a7c65b785a3005a8ae
Sai Kumar Kanuri <92247737+Sai-Kumar-Kanuri@users.noreply.github.com>
2024-06-15 22:00:22 +0530

    Time: 154 ms (37.38%), Space: 85.5 MB (63.02%) - LeetHub

---
+++ 0502-ipo/0502-ipo.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital) {
+
+        vector<pair<int,int>>arr;
+
+        for(int i=0;i<profits.size();i++){
+            arr.push_back({capital[i],profits[i]});
+        }
+
+
+        sort(arr.begin(),arr.end());
+
+        priority_queue<int>pq;
+
+        int i=0;
+
+        while(k--){
+
+            while(i<profits.size() && w>=arr[i].first){
+                pq.push(arr[i].second);
+                i++;
+            }
+
+
+            if(pq.empty()){
+                break;
+            }
+
+            w+=pq.top();
+            pq.pop();
+        }
+
+        return w;
+    }
+};
