806d3ad0f9cfe0cc3ad9d8fdfe4b0ef2d72ab327
Sai Kumar Kanuri <92247737+Sai-Kumar-Kanuri@users.noreply.github.com>
2025-07-15 20:57:48 +0530

    Time: 1 ms (99.12%), Space: 42 MB (80.13%) - LeetHub

---
+++ 3396-valid-word/3396-valid-word.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isValid(String word) {
+       
+       if(word.length()<3){
+        return false;
+       }
+
+
+       int vowelCount = 0;
+       int consonantCount = 0;
+
+       for(char c:word.toCharArray()){
+        if(!Character.isLetterOrDigit(c)){
+            return false;
+        }
+
+        if("aeiouAEIOU".indexOf(c)!=-1){
+            vowelCount++;
+        }else if((c>='a' && c<='z') || (c>='A' && c<='Z')){
+            consonantCount++;
+        }
+       }
+
+       return vowelCount>0 && consonantCount>0;
+    }
+}
