e4e900c2cd6b1f3ce5b5950a3fe1cab963f3ac52
Sai Kumar Kanuri <92247737+Sai-Kumar-Kanuri@users.noreply.github.com>
2026-06-13 13:20:49 +0530

    Time: 4 ms (45.03%), Space: 18.2 MB (42.15%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,53 +1,49 @@
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        
-        
-        vector<pair<int,int>>adj[n];//{to,cost}
-        
-        for(auto it:flights){
+        vector<pair<int,int>>adj[n];
+
+        for(auto it : flights){
             adj[it[0]].push_back({it[1],it[2]});
         }
-        
+
         vector<int>dist(n,1e9);
-        
-        
-        queue<pair<int,pair<int,int>>>q;//{stops,{node,dist}};
-        
+
+        priority_queue<pair<int,pair<int,int>>,
+                vector<pair<int,pair<int,int>>>,
+               greater<pair<int,pair<int,int>>>>pq;
+
+        pq.push({0,{0,src}});
+
         dist[src]=0;
-        q.push({0,{src,0}});
-        
-        
-        while(!q.empty()){
-            
-            auto it = q.front();
-            
-            int node = it.second.first;
-            int dis = it.second.second;
-            
+
+        while(!pq.empty()){
+            auto it = pq.top();
+            pq.pop();
             int stops = it.first;
-            q.pop();
-            
+
             if(stops>k){
                 continue;
             }
-            
-            for(auto it : adj[node]){
-                int adjNode = it.first;
-                int cost = it.second;
-                
-                if(dis+cost<dist[adjNode] && stops<=k){
-                    dist[adjNode]=dis+cost;
-                    q.push({stops+1,{adjNode,dis+cost}});
+
+            int cost = it.second.first;
+            int node = it.second.second;
+
+            for(auto itr : adj[node]){
+                int adjNode = itr.first;
+                int nCost = itr.second;
+
+                if(cost+nCost<dist[adjNode]){
+                    dist[adjNode]=cost+nCost;
+                    pq.push({stops+1,{cost+nCost,adjNode}});
                 }
             }
         }
-        
+
         if(dist[dst]==1e9){
             return -1;
         }
-        
-        return dist[dst] ;
-        
+
+        return dist[dst];
     }
 };
